a42e6a35e79764c4a882cc2c5814ba0458559fc4
emilyfloresfdez <113830301+emilyfloresfdez@users.noreply.github.com>
2022-12-07 14:44:48 -0600

    RECURSIVIDAD INT A STRING

---
+++ INTTOSTRING.java
@@ -0,0 +1,15 @@
+package RECURSIVIDAD;
+
+/* @author emily */
+
+public class INTTOSTRING 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("integerToString(89435):" +integerToString(89435));
+    }
+    public static String integerToString(int n)
+    {
+        return " "+n;
+    }
+}
