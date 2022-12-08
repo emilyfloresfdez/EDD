501b0d7f5d1087a4932f090369476de1c8b8538d
emilyfloresfdez <113830301+emilyfloresfdez@users.noreply.github.com>
2022-12-08 02:31:11 -0600

    Create Main.java

---
+++ CODIGO ARBOL BINARIO DE BUSQUEDA/Main.java
@@ -0,0 +1,37 @@
+package ARBOLBINARIOBUSQUEDA;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		ABB arbol = new ABB(new Nodo(8));
+		
+		Nodo n2 = new Nodo(5);
+		Nodo n3 = new Nodo(4);
+		Nodo n4 = new Nodo(3);
+		Nodo n5 = new Nodo(4);
+		Nodo n6 = new Nodo(9);
+		Nodo n7 = new Nodo(12);
+		Nodo n8 = new Nodo(19);
+		Nodo n9 = new Nodo(10);
+		
+		arbol.agregar(n2);
+		arbol.agregar(n3);
+		arbol.agregar(n4);
+		arbol.agregar(n5);
+		arbol.agregar(n6);
+		arbol.agregar(n7);
+		arbol.agregar(n8);
+		arbol.agregar(n9);
+		
+		arbol.imprimir();
+		System.out.println();
+		arbol.buscarRecursivo(20);
+		System.out.println();
+		arbol.eliminar(10);
+		arbol.imprimir();
+		System.out.println("FIN");
+	}
+
+}
