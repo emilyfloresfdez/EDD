9fa6646354ab892b225a0f5e34ee59d91db31420
emilyfloresfdez <113830301+emilyfloresfdez@users.noreply.github.com>
2022-12-08 02:22:14 -0600

    Create IPila.java

---
+++ CODIGOS COLA - PILA - LISTAS/CODIGO PILA/IPila.java
@@ -0,0 +1,22 @@
+package CODIGOCOLA;
+
+/* @author emily */
+public class main 
+{
+    public static void main(String []args)
+    {
+        Nodo n1 = new Nodo("Primer Nodo!!");
+        Nodo n2 = new Nodo("Segundo Nodo!!");
+        Nodo n3 = new Nodo(false);
+        Nodo n4 = new Nodo(55);
+        
+        Cola cola = new Cola(5);
+        
+        cola.agregar(n1);
+        cola.agregar(n2);
+        cola.agregar(n3);
+        cola.agregar(n4);
+        
+        cola.mostrar();
+    }
+}
