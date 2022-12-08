105849524b05024058c09daaaa16d231f4489c49
emilyfloresfdez <113830301+emilyfloresfdez@users.noreply.github.com>
2022-12-08 02:20:33 -0600

    Create Nodo.java

---
+++ CODIGOS COLA - PILA - LISTAS/CODIGO COLA/Nodo.java
@@ -0,0 +1,35 @@
+package CODIGOCOLA;
+
+/**
+ *
+ * @author emily
+ */
+public class Nodo 
+{
+    private Nodo siguiente;
+    private Object dato;
+    
+    public Nodo(Object dato)
+    {
+        this.dato = dato;
+        siguiente = null;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+    
+    
+}
